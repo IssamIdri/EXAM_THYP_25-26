34f7d5a610f6e1f3f25c1b3808e27af6f3275cbd
issam idrissi <issam.aissaouo87@gmail.com>
2025-12-09 09:38:14 +0100

    coorection

--- script.php
+++ script.php
@@ -1,19 +1,15 @@
 <?php
-// Script PHP simple
 echo "Bienvenue sur la page PHP !\n";
 echo "Examen THYP du 9 décembre 2025\n";
 echo "AISSAOUI IDRISSI ISSAM\n";
 
-// Fonction simple
 function afficherMessage($nom) {
     return "Bonjour, " . $nom . " !\n";
 }
 
-// Utilisation de la fonction
 $message = afficherMessage("ISSAM");
 echo $message;
 
-// Variable simple
 $date = "9 décembre 2025";
 echo "Date de l'examen : " . $date . "\n";
 ?>

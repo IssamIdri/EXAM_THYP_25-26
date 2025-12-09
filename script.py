8896360c238d47be13f75594444778ef2aeca13f
issam idrissi <issam.aissaouo87@gmail.com>
2025-12-09 09:35:54 +0100

    langages

---
+++ script.py
@@ -0,0 +1,20 @@
+# Script Python simple
+
+# Affichage de messages
+print("Bienvenue sur la page Python !")
+print("Examen THYP du 9 décembre 2025")
+print("AISSAOUI IDRISSI ISSAM")
+
+# Fonction simple
+def afficher_message(nom):
+    """Fonction qui affiche un message de bienvenue"""
+    return f"Bonjour, {nom} !"
+
+# Utilisation de la fonction
+message = afficher_message("ISSAM")
+print(message)
+
+# Variable simple
+date = "9 décembre 2025"
+print(f"Date de l'examen : {date}")
+
